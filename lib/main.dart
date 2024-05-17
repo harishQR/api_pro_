e30f5b48f597725318ec7e4e29cfef2de85b122f
harishQR <159750945+harishQR@users.noreply.github.com>
2024-05-17 16:19:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,80 @@
+import 'dart:convert';
+import 'package:api_pro/model/user.dart';
 import 'package:flutter/material.dart';
-
-import 'home.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+class home extends StatefulWidget {
+  const home({super.key});
+  @override
+  State<home> createState() => _homeState();
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class _homeState extends State<home> {
+List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: home(),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          flexibleSpace:Container(
+            height: 600,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey.shade300,
+            ),
+
+          ),
+        title: Text("User Data",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,),),
+          centerTitle: true,
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+          width:MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Container(
+                  width : MediaQuery.of(context).size.width,
+                  height: 800,
+                  child: ListView.builder(itemCount: users.length,itemBuilder: (context,index){
+                    final user = users[index];
+                    final name = user['name']['first'];
+                    final email = user['email'];
+                    final img = user['picture']['thumbnail'];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(img)),
+                      ),
+                      title: Text(name,style: TextStyle(fontWeight: FontWeight.bold),),
+                      subtitle: Text(email),
+                    );
+                  }),
+                )
+              ],
+            ),
+        ),
+          Positioned(
+            bottom: 5,
+            right: 5,
+            child:
+            FloatingActionButton(onPressed: fetchuser,child: Icon(Icons.add,),backgroundColor: Colors.lightGreen ,),
+          ),
+          ],
+      ),
     );
   }
+  void fetchuser() async{
+    print("data fetched...");
+   const url = "https://randomuser.me/api/?results=25";
+    final uri = Uri.parse(url);
+  final res = await http.get(uri);
+  final body = res.body;
+  final json = jsonDecode(body);
+   setState(() {
+        users = json['results'];
+   });
+  }
 }
-
-
